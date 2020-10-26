37347e8dc18a878b67c80af640f21d3c5b1237ee
Whasnat <w.hasnat94@gmail.com>
2020-10-25 22:02:44 -0700

    python 3.9 upgrade

--- users/forms.py
+++ users/forms.py
@@ -1,9 +1,23 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField()  #adds extra EmailField to the Built-in Django-Forms 
     class Meta:
         model = User        
         fields = ['username', 'email', 'password1', 'password2']       # Pass the required fields 
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
